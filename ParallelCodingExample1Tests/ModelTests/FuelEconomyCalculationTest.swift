22b2bbf940273e72479be3e37e6ebad81bea0af4
Karl Becker <karl@karlbecker.com>
2018-08-05 22:14:26 -0500

    Add a failing & skipped test to calculate the L/100km fuel economy calculation

--- ParallelCodingExample1Tests/ModelTests/FuelEconomyCalculationTest.swift
+++ ParallelCodingExample1Tests/ModelTests/FuelEconomyCalculationTest.swift
@@ -23,6 +23,16 @@ class FuelEconomyCalculationTestSpec: QuickSpec {
                     
                     expect(entry2.fuelEconomyIn(unit: .mpg, previousEntry: entry1)).to(beCloseTo(5.0, within: 0.001))
                 })
+                
+                
+                xit("should calculate 56.5 L/100km when the entries are 100 miles apart and the fuel is 20 gallons", closure: {
+                    let entry1 = FuelEntry(fuelType: "normal", isFullTank: true, resetFuelEconomy: false, quantity: 1.0, odometer: 100.1, date: Date(), notes: "", location: nil, cost: 2.599, currency: "$", quantityUnits: .Gallons)
+                    let entry2 = FuelEntry(fuelType: "normal", isFullTank: true, resetFuelEconomy: false, quantity: 20.0, odometer: 200.1, date: Date(), notes: "", location: nil, cost: 2.599, currency: "$", quantityUnits: .Gallons)
+                    
+                    expect(entry2.fuelEconomyIn(unit: .lper100km, previousEntry: entry1)).to(beCloseTo(56.5, within: 0.001))
+                })
+                
+                
             })
         }
     }
